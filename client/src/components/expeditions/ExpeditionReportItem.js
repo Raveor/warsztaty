8b877fe975c6e258afd941d65620d1f0e041f39d
Jan Podsiadło <janpodsiadlo95@gmail.com>
2019-05-15 01:28:04 +0200

    Expedition report display components

--- client/src/components/expeditions/ExpeditionReportItem.js
+++ client/src/components/expeditions/ExpeditionReportItem.js
@@ -1,23 +1,48 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class ExpeditionReportItem extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
     render() {
-        return (
-            <div>
-                {JSON.stringify(this.props.report)}
+        let report = this.props.report;
+        let endDate = new Date(
+            new Date(report.whenStarted).getTime() + report.time
+        ).toLocaleString();
+        let content = this.props.expanded ? (
+            <div className="collection-item">
+                <span
+                    className="title"
+                    onClick={() => this.props.expandFunc(null)}
+                >
+                    <h4>{report.name}</h4>
+                </span>
+                <p>Level {report.level}</p>
+                <p>
+                    Loot:<br />
+                    {report.moneyPrize || 0} gold<br />
+                    {report.itemPrize || ""}<br />
+                    <i className="grey-text">{endDate}</i>
+                </p>
             </div>
+        ) : (
+            <a
+                href="#!"
+                className="collection-item"
+                onClick={() => this.props.expandFunc(report._id)}
+            >
+                <div>
+                    {report.name}
+                    <div className="secondary-content">{endDate}</div>
+                </div>
+            </a>
         );
+        return content;
     }
 }
 
 ExpeditionReportItem.propTypes = {
     expanded: PropTypes.bool,
-    report: PropTypes.object.isRequired
+    report: PropTypes.object.isRequired,
+    expandFunc: PropTypes.func.isRequired
 };
 
 export default ExpeditionReportItem;
--- client/src/components/expeditions/ExpeditionReportList.js
+++ client/src/components/expeditions/ExpeditionReportList.js
@@ -1,29 +1,91 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import axios from "axios";
 import ExpeditionReportItem from "./ExpeditionReportItem";
 
 class ExpeditionReportList extends Component {
-    componentDidMount() {
-        axios.get(`/expeditions/reports`).then(res => {
-            const reports = res.data;
-            this.setState({ reports });
-        });
+    constructor(props) {
+        super(props);
+        this.state = { reports: [], expandedReport: null };
     }
 
+    componentDidMount() {
+        this.loadReports();
+    }
+
+    loadReports = () => {
+        axios.get(`/expeditions/reports`).then(res => {
+            const reports = res.data/* .concat(testReports) */; // testing displaying reports when unable to pull them from backend
+            const expandedReport = reports.length > 0 ? reports[0]._id : null;
+            this.setState({ reports, expandedReport });
+        });
+    };
+
+    setExpanded = id => {
+        this.setState({ expandedReport: id });
+    };
+
     render() {
+        let reports =
+            this.state.reports && this.state.reports.length > 0 ? (
+                this.state.reports.map(report => (
+                    <ExpeditionReportItem
+                        key={report._id}
+                        report={report}
+                        expanded={report._id === this.state.expandedReport}
+                        expandFunc={this.setExpanded}
+                    />
+                ))
+            ) : (
+                <p className="collection-item">No expedition reports yet!</p>
+            );
         return (
-            <div>
-                <ul>
-                    {this.state.reports.map((report, index) =>
-                        <ExpeditionReportItem report={report}></ExpeditionReportItem>
-                    )}
-                </ul>
+            <div className="container" style={{ backgroundColor: "white" }}>
+                <div className="collection">
+                    <div className="collection-item collection-header">
+                        <h3>Expedition reports</h3>
+                    </div>
+                    {reports}
+                </div>
             </div>
         );
     }
 }
 
-ExpeditionReportList.propTypes = {};
+const testReports = [
+    {
+        _id: 1,
+        name: "Test expedition 1",
+        time: 12345,
+        level: 1,
+        whenStarted: new Date(new Date().getTime() - 100000),
+        moneyPrize: 1000,
+        itemPrize: "test reward 1"
+    },
+    {
+        _id: 2,
+        name: "Test expedition 2",
+        time: 22345,
+        level: 2,
+        whenStarted: new Date(new Date().getTime() - 100000),
+        itemPrize: "test reward 2"
+    },
+    {
+        _id: 3,
+        name: "Test expedition 3",
+        time: 32345,
+        level: 3,
+        whenStarted: new Date(new Date().getTime() - 100000),
+        moneyPrize: 2000
+    },
+    {
+        _id: 4,
+        name: "Test expedition 4",
+        time: 42345,
+        level: 4,
+        whenStarted: new Date(new Date().getTime() - 100000),
+        moneyPrize: 3000,
+        itemPrize: "test reward 3"
+    }
+];
 
 export default ExpeditionReportList;
